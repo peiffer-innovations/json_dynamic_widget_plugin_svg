fba7b14d9e377ad98991e466b0de82e9e1c1f5fe
action <noop@github.com>
2023-03-28 02:37:48 +0000

    Pages 50

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "f86fd438c95747f65c818ae738478e64",
+"main.dart.js": "64ab3061ad1a020a8cc981d27831bd3d",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/NOTICES": "892db772a8c9a756f6e3d01a2e18bc90",
-"index.html": "ecdbdff092c8d23599332fe70b37c16a",
-"/": "ecdbdff092c8d23599332fe70b37c16a",
+"index.html": "e98ae7022cae51cb6881fdcd8aa1045a",
+"/": "e98ae7022cae51cb6881fdcd8aa1045a",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
 };
